518c965008a29fbe63dac468aaec6b26ab47cc12
João Diego <jdflorencio@gmail.com>
2020-02-03 15:05:16 -0300

    Agregação

--- capt2/classes/Produto.class.php
+++ capt2/classes/Produto.class.php
@@ -6,10 +6,12 @@ class Produto
     var $Despedida;
     var $Preco;
     var $Quantidade;
+    
 
     function ImprimeEtiqueta()
     {
         print 'Código:     '. $this->Codigo . "<br />";
         print 'Descrição:     '. $this->Descricao . "<br />";
+        echo '---------------------------------------- <br />';
     }
 }
